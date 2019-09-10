a0108af9687b620dd3e315bbad63e5d6753f8494
Vassi Gianitsos <vgianitsos@yahoo.com>
2019-09-10 15:37:46 -0500

    IBOutlet properties & IBAction functions

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // 4 IB outlet properties below
+    //obj hooks up item in story board to here
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //IB action functions
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        print("deal tapped!!")
+        
+    }
+    
+    
+    
+    
 }
 
