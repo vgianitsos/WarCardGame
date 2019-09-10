137566bae1e73aaf26f5711e40a355d853f94af7
Vassi Gianitsos <vgianitsos@yahoo.com>
2019-09-10 14:23:55 -0500

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Vassi Gianitsos on 9/10/19.
+//  Copyright © 2019 Vassi Gianitsos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
